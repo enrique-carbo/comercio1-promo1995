d031491ca6b5d9b854810b852a9d31b6056b421a
ecarbo <enriquecarbo@tutanota.com>
2025-10-11 10:33:08 -0300

    Config Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/comercio1-promo1995/', 
     server: {
       port: 3000,
       host: '0.0.0.0',
