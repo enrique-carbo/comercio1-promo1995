88a166d08a395074c6657d9d801fa2fcc9fc5695
ecarbo <enriquecarbo@tutanota.com>
2025-10-12 20:17:45 -0300

    change base in vit.config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/comercio1-promo1995/', 
+    base: '/', 
     server: {
       port: 3000,
       host: '0.0.0.0',
